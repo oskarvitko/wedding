1f9b342c468d5470b537c9cdee7ab92aecccc5d5
Oskar-Vitko1 <oskar.vitko1@ibm.com>
2022-04-16 22:10:54 +0300

    fix

--- assets/data/titles.js
+++ assets/data/titles.js
@@ -34,7 +34,7 @@ const includeAdminMode = () => {
   <div class="container text-center mt-5">
   ${titles
     .map((title) => {
-      const publicUrl = `${url}?id=${title.id}`;
+      const _url = `${publicUrl}?id=${title.id}`;
       return `
       <h3 style="font-style: normal;" class="my-4">
       <b>${title.name}:</b> 
